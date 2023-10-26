f1094f316ea8880ff8b0474d86971a631e12bc2e
kojesung <90428805+kojesung@users.noreply.github.com>
2023-10-26 15:30:08 +0900

    Create App.js

---
+++ week03/ggo_jae/App.js
@@ -0,0 +1,14 @@
+import { movies } from "./Components/movieDummy";
+import Movie from "./Components/Movie"
+function App() {
+  return (
+    <div className="app-container">
+      {
+        <Movie />
+      }
+
+    </div>
+  );
+}
+
+export default App;
