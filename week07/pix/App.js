209401c0987634bf61eff0b8dadfc4003f5cb49c
yawn11 <86337988+yawn11@users.noreply.github.com>
2023-12-04 14:04:58 +0900

    7주차 미션

---
+++ week07/pix/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Header from "./components/Header";
+import Celebrity from "./pages/Celebrity";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieDetail from "./pages/MovieDetail";
+import NotFound from "./pages/NotFound";
+import Tvtv from "./pages/Tv";
+import TvDetail from "./pages/TvDetail";
+
+function App() {
+  return (
+    <div className="root-wrap">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie" element={<Movies />} />
+          <Route path="/movie/:title" element={<MovieDetail />} />
+          <Route path="/tv" element={<Tvtv />} />
+          <Route path="/tv/:name" element={<TvDetail />} />
+          <Route path="/person" element={<Celebrity />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
