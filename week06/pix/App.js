9a2ad89882e5e4be39cabec1c4af155fc1f09a0f
yawn11 <86337988+yawn11@users.noreply.github.com>
2023-12-06 23:37:53 +0900

    6주차 미션

--- week06/pix/App.js
+++ week06/pix/App.js
@@ -5,7 +5,11 @@ import Home from "./pages/Home";
 import Movies from "./pages/Movies";
 import MovieDetail from "./pages/MovieDetail";
 import NotFound from "./pages/NotFound";
-import Tv from "./pages/Tv";
+import Tvtv from "./pages/Tv";
+import TvDetail from "./pages/TvDetail";
+import LoginPage from "./pages/LoginPage";
+import LogoutPage from "./pages/LogoutPage";
+
 
 function App() {
   return (
@@ -16,9 +20,12 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/movie" element={<Movies />} />
           <Route path="/movie/:title" element={<MovieDetail />} />
-          <Route path="/tv" element={<Tv />} />
+          <Route path="/tv" element={<Tvtv />} />
+          <Route path="/tv/:name" element={<TvDetail />} />
           <Route path="/person" element={<Celebrity />} />
           <Route path="/*" element={<NotFound />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/logout" element={<LogoutPage />} />
         </Routes>
       </BrowserRouter>
     </div>
