46ed7f134951caffe54203b953fab28fe5c2847a
yawn11 <86337988+yawn11@users.noreply.github.com>
2023-12-06 23:39:22 +0900

    6주차 미션

--- week06/pix/pages/TvDetail.jsx
+++ week06/pix/pages/TvDetail.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom';
-import { IMG_BASE_URL } from '../components/Movie/Movie';
+import { IMG_BASE_URL } from '../components/Tv';
 
-export default function MovieDetail() {
+export default function TvDetail() {
     const {state} = useLocation();
 
     return (
@@ -11,10 +11,10 @@ export default function MovieDetail() {
                 <img 
                     style={{ width: "300px", height: "450px" }}
                     src={IMG_BASE_URL + state.poster_path}
-                    alt='영화 포스터 이미지'
+                    alt='tv 포스터 이미지'
                 />
                 <div>
-                    <div style={{ fontSize: "32px" }}>{state.title}</div>
+                    <div style={{ fontSize: "32px" }}>{state.name}</div>
                     <br />
                     <div style={{ fontSize: "20px" }}>{state.overview}</div>
                 </div>
